9d207739016b3773746e2a2036341a49cecfe6d6
BOUNEHMEElie <e.bounehme@equadis.com>
2024-08-31 13:15:23 +0300

    .

--- resources/views/users/include/head.blade.php
+++ resources/views/users/include/head.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 
-	<!-- META -->
+    <!-- META -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="keywords" content="" />
@@ -10,6 +10,74 @@
     <meta name="robots" content="" />    
     <meta name="description" content="" />
     
+    <!-- Google Search Console Verification -->
+    <meta name="google-site-verification" content="BjPxPtJqG4-yydmxzDT8jt1VKk4cEDFSdcMOaihHm_E" />
+
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-22BCWB35VR"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-22BCWB35VR');
+    </script>
+
+    <!-- Contact Address Schema -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "LocalBusiness",
+        "name": "Locum Health",
+        "image": "https://locumhealthuk.com/images/logo.gif",
+        "url": "https://locumhealthuk.com/",
+        "telephone": "02039315313",
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "152-160 City Road, Kemp House",
+            "addressLocality": "London",
+            "postalCode": "EC1V 2NX",
+            "addressCountry": "GB"
+        },
+        "sameAs": [
+            "https://www.facebook.com/LocumHealthUK/",
+            "https://www.instagram.com/locumhealth_uk/",
+            "https://twitter.com/LocumHealthUK",
+            "https://www.linkedin.com/company/locum-health-uk"
+        ]
+    }
+    </script>
+
+    <!-- Organization Schema -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Locum Health",
+        "alternateName": "locumhealth",
+        "url": "https://locumhealthuk.com/",
+        "logo": "https://locumhealthuk.com/images/logo.gif",
+        "sameAs": [
+            "https://www.facebook.com/LocumHealthUK/",
+            "https://twitter.com/LocumHealthUK?t=IrlV9uAPwTwoC9ix28u9TA&s=09",
+            "https://www.instagram.com/locumhealth_uk/",
+            "https://www.linkedin.com/company/locum-health-uk"
+        ]
+    }
+    </script>
+
+    <!-- Image Schema -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "ImageObject",
+        "author": "Locum Health",
+        "contentUrl": "https://locumhealthuk.com/images/logo.gif",
+        "thumbnail": "https://locumhealthuk.com/images/logo.gif",
+        "description": "Locum Health UK is a renowned locum medical jobs recruitment agency in the UK specializing in primary care. We offer the best locum jobs and same-day payments.",
+        "name": "locumhealth"
+    }
+    </script>
+
     <!-- FAVICONS ICON -->
     <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
@@ -20,33 +88,28 @@
     <!-- MOBILE SPECIFIC -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"><!-- BOOTSTRAP STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"><!-- FONTAWESOME STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/feather.css"><!-- FEATHER ICON SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css"><!-- OWL CAROUSEL STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css"><!-- MAGNIFIC POPUP STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/lc_lightbox.css"><!-- Lc light box popup -->     
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css"><!-- BOOTSTRAP SLECT BOX STYLE SHEET  -->
-    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap5.min.css"><!-- DATA table STYLE SHEET  -->
-    <link rel="stylesheet" type="text/css" href="css/select.bootstrap5.min.css"><!-- DASHBOARD select bootstrap  STYLE SHEET  -->     
-    <link rel="stylesheet" type="text/css" href="css/dropzone.css"><!-- DROPZONE STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/scrollbar.css"><!-- CUSTOM SCROLL BAR STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/datepicker.css"><!-- DATEPICKER STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/flaticon.css"> <!-- Flaticon -->
-    <link rel="stylesheet" type="text/css" href="css/swiper-bundle.min.css"><!-- Swiper Slider -->
-    <link rel="stylesheet" type="text/css" href="css/style.css"><!-- MAIN STYLE SHEET -->
-
-    <!-- THEME COLOR CHANGE STYLE SHEET -->
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/feather.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lc_lightbox.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" type="text/css" href="css/select.bootstrap5.min.css">
+    <link rel="stylesheet" type="text/css" href="css/dropzone.css">
+    <link rel="stylesheet" type="text/css" href="css/scrollbar.css">
+    <link rel="stylesheet" type="text/css" href="css/datepicker.css">
+    <link rel="stylesheet" type="text/css" href="css/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" class="skin" type="text/css" href="css/skins-type/skin-6.css">
-    <!-- SIDE SWITCHER STYLE SHEET -->
-    <link rel="stylesheet" type="text/css" href="css/switcher.css">  
+    <link rel="stylesheet" type="text/css" href="css/switcher.css">
 
-
-<style>
-
-.wt-bnr-inr{
-  max-height:150px;
-}
-</style>
+    <style>
+        .wt-bnr-inr {
+            max-height: 150px;
+        }
+    </style>
 </head>
